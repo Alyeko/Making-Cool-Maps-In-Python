e06d58ffd142ddf20a95b707d5d5c508454c0359
Alberta Odamea Anim-Ayeko <78666634+Alyeko@users.noreply.github.com>
2021-03-13 09:39:05 +0000

    Update app.py
    Deleted the route "/map" because I wanted the map to be displayed at the route "/".
    Deleted the first three lines of the index function because it wasn't necessary.
    Changed the return statement to return 'the_map.html'

--- app.py
+++ app.py
@@ -5,14 +5,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    start_coords = (5.6, 26.17)
-    folium_map = folium.Map(location=start_coords, zoom_start=3.4)
-    folium_map.save('templates/map.html')
-    return render_template('index.html')
-
-
-@app.route('/map')
-def map():
     return render_template('the_map.html')
 
 if __name__ == '__main__':
