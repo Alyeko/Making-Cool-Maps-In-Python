bd968f8448ce0922e219f000174216dd2a5bc0a9
Alberta Odamea Anim-Ayeko <78666634+Alyeko@users.noreply.github.com>
2021-03-13 14:20:43 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def the_map():
+def render_the_map():
     return render_template('the_map.html')
 
 if __name__ == '__main__':
